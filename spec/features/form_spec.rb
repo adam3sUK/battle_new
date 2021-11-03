56fd8525740e716ec76c4abc410cd5b0b65a7758
Adam Harbisher <aharbisher@gmail.com>
2021-11-03 14:56:57 +0000

    DRY'd code with helpers

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -1,9 +1,6 @@
 feature 'form' do
   scenario 'completion of form leads to new page that displays name' do
-    visit('/')
-    fill_in 'player_1', with: 'Bob'
-    fill_in 'player_2', with: 'Rob'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Bob vs Rob'
   end
 end
--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -1,9 +1,6 @@
 feature 'hit points' do
   scenario 'display hit points' do
-    visit('/')
-    fill_in 'player_1', with: 'Bob'
-    fill_in 'player_2', with: 'Rob'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Bob hp: 50/50, Rob hp: 50/50'
   end
 end
